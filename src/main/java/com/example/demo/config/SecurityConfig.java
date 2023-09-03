6ef708d0eef5e3167ed2d351b26dead52d5633b8
usingjun <97494494+usingjun@users.noreply.github.com>
2023-09-03 23:49:41 +0900

    edit SecurityConfig imports and remove headers code

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,25 +1,14 @@
 package com.example.demo.config;
 
 import com.example.demo.service.OAuth2MemberService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 // 스프링 시큐리티 설정 클래스
 @EnableMethodSecurity
@@ -27,13 +16,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-
-    private final ObjectMapper objectMapper;
-
-
-
-
-
 
     private final OAuth2MemberService oAuth2MemberService;
 
@@ -49,7 +31,6 @@ public class SecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/admin/**")).access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
                 .and()
-                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))) // 추가된 부분
                 .formLogin()
                 .loginPage("/user/login")
                 .loginProcessingUrl("/user/login")
